e8911cc9cdb48e7ca6f8d3b5503864900faf5282
Afshrsgnd <124483567+Afshrsgnd@users.noreply.github.com>
2023-02-06 13:20:51 +0330

    Add files via upload

---
+++ adad zouj.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	
+	int i;
+	
+	int x=0;
+	
+	cout<<"Enter a number : ";
+	
+	cin>>a;
+	
+	if (a%2==0){
+	
+	 x++;}else{
+	 	x=0;
+	 }
+	
+	while (x<1 ){
+	 	
+
+	 	cout<<"Enter a number : ";
+	 	
+	 		
+	cin>>a;
+	
+	if (a%2==0)
+	{
+	
+	x++;
+	
+	}else{
+	
+	x=0;
+	     }
+	 
+
+}
+	return 0; 
+
+}
